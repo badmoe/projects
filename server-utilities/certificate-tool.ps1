c5481ad9d5f149eb6f0b6ade59e4085ce6baa6ff
ben <57734768+badmoe@users.noreply.github.com>
2023-02-14 08:27:13 -0600

    Update certificate-tool.ps1

--- server-utilities/certificate-tool.ps1
+++ server-utilities/certificate-tool.ps1
@@ -1,4 +1,3 @@
-# Function to export personal certificates to C:\certs\
 Function Export-PersonalCertificates {
     If (!(Test-Path "C:\certs\")) {
         New-Item -ItemType Directory -Path "C:\certs\"
@@ -24,7 +23,6 @@ Function Export-PersonalCertificates {
     }
 }
 
-# Function to check certificates last use
 Function Check-CertificateLastUse {
     $certs = Get-ChildItem -Path "C:\certs\"
     
@@ -38,57 +36,75 @@ Function Check-CertificateLastUse {
     }
 }
 
-# Function to compare certificate contents
-Function Compare-CertificateContents {
-    $certs = Get-ChildItem -Path "C:\certs\"
-    
-    For ($i = 0; $i -lt $certs.Count; $i++) {
-        For ($j = $i + 1; $j -lt $certs.Count; $j++) {
-            $cert1 = Get-PfxCertificate -FilePath $certs[$i].FullName
-            $cert2 = Get-PfxCertificate -FilePath $certs[$j].FullName
-            
-            If ($cert1.Subject -ne $cert2.Subject) {
-                Write-Host "Subject Name differences: $($certs[$i].Name) vs $($certs[$j].Name)"
-            }
-            
-            If ($cert1.Issuer -ne $cert2.Issuer) {
-                Write-Host "Issuer Name differences: $($certs[$i].Name) vs $($certs[$j].Name)"
-            }
-            
-            If ($cert1.SerialNumber -ne $cert2.SerialNumber) {
-                Write-Host "Serial Number differences: $($certs[$i].Name) vs $($certs[$j].Name)"
-            }
-            
-            If ($cert1.Thumbprint -ne $cert2.Thumbprint) {
-                Write-Host "Thumbprint differences: $($certs[$i].Name) vs $($certs[$j].Name)"
-            }
+Function Compare-CertificateDetails {
+    $certs = Get-ChildItem -Path "C:\certs\" -Filter *.cer | Import-Certificate
+    $certDetails = @()
+
+    Foreach ($cert in $certs) {
+        $subject = $cert.Subject
+        $san = $cert.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Subject Alternative Name" } | Select-Object -ExpandProperty Format(0)
+        $ku = $cert.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Key Usage" } | Select-Object -ExpandProperty Format(0)
+
+        $certDetails += [PSCustomObject]@{
+            Subject = $subject
+            SAN = $san
+            KU = $ku
+        }
+    }
+
+    For ($i = 0; $i -lt $certDetails.Count; $i++) {
+        For ($j = $i + 1; $j -lt $certDetails.Count; $j++) {
+            Write-Host "Comparing certificate $($i + 1) to certificate $($j + 1):"
+            If ($certDetails[$i].Subject -ne $certDetails[$j].Subject) { Write-Host "Subject differs: $($certDetails[$i].Subject) vs $($certDetails[$j].Subject)" }
+            If ($certDetails[$i].SAN -ne $certDetails[$j].SAN) { Write-Host "Subject Alternative Name differs: $($certDetails[$i].SAN) vs $($certDetails[$j].SAN)" }
+            If ($certDetails[$i].KU -ne $certDetails[$j].KU) { Write-Host "Key Usage differs: $($certDetails[$i].KU) vs $($certDetails[$j].KU)" }
+            Write-Host ""
         }
     }
 }
 
-# Function to combine all certificates into one "master-cert.pfx"
+
 Function Combine-Certificates {
     $certs = Get-ChildItem -Path "C:\certs\" -Filter *.cer
     $certs | Export-PfxCertificate -FilePath "C:\certs\master-cert.pfx" -Password (ConvertTo-SecureString -String "password" -Force -AsPlainText)
 }
 
+Function Show-CertificateDetails {
+    $certs = Get-ChildItem -Path "C:\certs\" -Filter *.cer | Import-Certificate
 
-# Main loop to prompt the user for a function to run
+    Write-Host "Please select which certificate details to display:"
+    Write-Host "1. Subject"
+    Write-Host "2. Subject Alternative Name"
+    Write-Host "3. Key Usage"
+    $details = Read-Host "Enter the numbers of the certificate details you would like to display, separated by a comma (e.g. 1,2,3): "
+
+    $details = $details -split ","
+
+    Foreach ($cert in $certs) {
+        If ($details -contains "1") { Write-Host "Subject: $($cert.Subject)" }
+        If ($details -contains "2") { Write-Host "Subject Alternative Name: $($cert.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Subject Alternative Name" } | Select-Object -ExpandProperty Format(0))" }
+        If ($details -contains "3") { Write-Host "Key Usage: $($cert.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Key Usage" } | Select-Object -ExpandProperty Format(0))" }
+        Write-Host ""
+    }
+}
+
 While ($true) {
     Write-Host "Please select a function to run:"
     Write-Host "1. Export Personal Certificates"
     Write-Host "2. Check Certificate Last Use"
     Write-Host "3. Compare Certificate Contents"
-    Write-Host "4. Combine Scripts"
-    Write-Host "5. Quit Script"
+    Write-Host "4. Combine Crtificates"
+    Write-Host "5. Certificate Details"
+    Write-Host "6. Quit Script"
     $function = Read-Host "Enter the number of the function you would like to run: "
     
     Switch ($function) {
         1 { Export-PersonalCertificates }
         2 { Check-CertificateLastUse }
-        3 { Compare-CertificateContents }
+        3 { Compare-CertificateDetails }
         4 { Combine-Certificates }
-        5 { Break }
+        5 { Show-CertificateDetail }
+        6 { Break }
         Default { Write-Host "Invalid selection. Please try again." }
     }
     
