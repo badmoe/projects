68bd0638b5ca41501405b78160b54405d95e4252
ben <57734768+badmoe@users.noreply.github.com>
2023-03-08 12:25:14 -0600

    Update tslogbackup.ps1

--- server-utilities/tslogbackup.ps1
+++ server-utilities/tslogbackup.ps1
@@ -7,8 +7,15 @@ $documentsPath = [Environment]::GetFolderPath("MyDocuments")
 # Define the path to the Transcripts folder
 $transcriptsPath = Join-Path $documentsPath "Transcripts"
 
-# Find all folders in the Documents folder that contain files matching the search pattern
-$foldersToZip = Get-ChildItem $documentsPath -Directory | Where-Object { (Get-ChildItem $_.FullName -File -Filter $searchPattern).Count -gt 0 }
+# Define the name of the folder to skip based on today's date
+$skipFolderName = (Get-Date).ToString("yyyyMMdd")
+
+# Find all folders in the Documents folder that contain files matching the search pattern,
+# except for the folder matching today's date
+$foldersToZip = Get-ChildItem $documentsPath -Directory | Where-Object {
+    ($_.Name -ne $skipFolderName) -and
+    (Get-ChildItem $_.FullName -File -Filter $searchPattern).Count -gt 0
+}
 
 # For each folder to zip, create a zip file in the Transcripts folder with the same name and delete the original folder
 foreach ($folder in $foldersToZip) {
